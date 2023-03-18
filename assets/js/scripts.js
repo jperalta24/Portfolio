ec1217a358d0cad81dcd4293d7ec0fdeb9c7ec63
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-03-18 16:35:52 -0700

    new look

---
+++ assets/js/scripts.js
@@ -0,0 +1,48 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+  card.addEventListener('mouseover', () => {
+    cards.forEach(c => c.classList.remove('enlarge-card'));
+    card.classList.add('enlarge-card');
+  });
+
+  card.addEventListener('mouseout', () => {
+    // remove the classes that were added on mouseover
+    card.classList.remove('enlarge-card');
+  });
+
+});
